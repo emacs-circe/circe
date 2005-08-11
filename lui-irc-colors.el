43c35bc0c28f943840e3d9550363490795224759
forcer <forcer>
2005-08-11 23:42:28 +0000

    lui-irc-colors.el: Project name eircc -> circe

--- lui-irc-colors.el
+++ lui-irc-colors.el
@@ -5,7 +5,7 @@
 ;; Version: 1.0
 ;; Keywords: Lui, CIRCE, IRC
 ;; Author: Jorgen Schaefer <forcer@forcix.cx>
-;; URL: http://www.nongnu.org/eircc/
+;; URL: http://www.nongnu.org/circe/
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
