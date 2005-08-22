e22e9d5addaab6cbdc95d92e1b8a16e1810582fc
bpalmer <bpalmer>
2005-08-22 07:06:42 +0000

    * lui-xemacs.el (string-to-number): Added advice so that xemacs will, if over- or under-flow occurs when converting a string to an integer, it attempts to convert to floating-point instead. This fixes an issue with channel timestamps crashing xemacs.

--- lui-xemacs.el
+++ lui-xemacs.el
@@ -5,6 +5,7 @@
 
 (unless (fboundp 'propertize)
   (defun propertize (string &rest props)
+    ;; stolen from erc's erc-propertize
     (let ((str (copy-sequence string)))
       (while props
         (put-text-property 0 (length string)
@@ -19,5 +20,47 @@
   (defun float-time (&optional specified-time)
     (time-to-seconds (or specified-time
                          (current-time)))))
+
+(unless (fboundp 'remove-overlays)
+  (defun remove-overlays (&optional beg end name val)
+    "Clear BEG and END of overlays whose property NAME has value VAL.
+Overlays might be moved and or split. "
+    ;; Stolen from planner as planner-remove-overlays
+    (if (< end beg)
+        (setq beg (prog1 end (setq end beg))))
+    (save-excursion
+      (dolist (o (overlays-in beg end))
+        (when (eq (overlay-get o name) val)
+          ;; Either push this overlay outside beg...end
+          ;; or split it to exclude beg...end
+          ;; or delete it entirely (if it is contained in beg...end).
+          (if (< (overlay-start o) beg)
+              (if (> (overlay-end o) end)
+                  (progn
+                    (move-overlay (planner-copy-overlay o)
+                                  (overlay-start o) beg)
+                    (move-overlay o end (overlay-end o)))
+                (move-overlay o (overlay-start o) beg))
+            (if (> (overlay-end o) end)
+                (move-overlay o end (overlay-end o))
+              (delete-overlay o))))))))
+
+(defvar lui-string-to-number-sentinel nil
+  "Internal variable used to track whether inside lui-string-to-number")
+(defadvice string-to-number (around lui-string-to-number activate)    
+  (if lui-string-to-number-sentinel
+      ad-do-it
+    (let* ((lui-string-to-number-sentinel t)
+	   (data (ad-get-arg 0))
+	   (positive-number-regexp "^\\s *\\+?[0-9.]")
+	   (posnump (string-match positive-number-regexp data))
+	   (number ad-do-it))
+      (setq ad-return-value (if (or (floatp number)
+				    (zerop number))
+				number
+			      (if (or (and posnump (< number 0))
+				      (and (not posnump) (> number 0)))
+				  (string-to-number (concat data ".0"))
+				number))))))
 
 (provide 'lui-xemacs)
