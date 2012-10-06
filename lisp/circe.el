558849eb134efcb446b7e04d4070978abe8326a0
Pi <pi+circe@pihost.us>
2012-10-05 21:27:00 -0700

    circe.el: allow c-c-INVITE to be used without specifying the channel name.
    This improves interactive use.

--- lisp/circe.el
+++ lisp/circe.el
@@ -1765,8 +1765,16 @@ consisting of two words, the nick and the channel."
     (if (string-match "^\\([^ ]+\\) +\\([^ ]+\\)" nick)
         (setq channel (match-string 2 nick)
               nick (match-string 1 nick))
-      (circe-server-message "Usage: /INVITE <nick> <channel>")))
-  (circe-server-send (format "INVITE %s %s" nick channel)))
+      (when (or (and (null channel)
+                     (string= "" nick))
+                (and (null channel)
+                     (null nick)))
+        (circe-server-message "Usage: /INVITE <nick> <channel>")))
+  (circe-server-send (format "INVITE %s %s" nick
+                             (if (and (null channel)
+                                      (not (null nick)))
+                                 circe-chat-target
+                               channel)))))
 
 (defun circe-command-SV (&optional ignored)
   "Tell the current channel about your client and Emacs version."
