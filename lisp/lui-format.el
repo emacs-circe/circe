509a58c44a798fe499d3bdb48ca888d419654047
Jorgen Schaefer <forcer@forcix.cx>
2012-10-13 11:51:02 +0200

    lui-format: Support formatting options.
    lui-format now supports all formatting options of `format'. The
    following two expressions are equivalent:
    
      (lui-format "{pi:.2f} chart" :pi 3.14159)
    
      (format "%.2f chart" 3.14159)
    
    Also, escaping has been moved from backslashes to doubling the
    escaped character, so (lui-format "{{foo}}") => "{foo}".
    
    Finally, the license was changed to GPLv3 (see issue #23).

--- lisp/lui-format.el
+++ lisp/lui-format.el
@@ -1,6 +1,6 @@
 ;;; lui-format.el --- A formatting function for use with Lui
 
-;; Copyright (C) 2005  Jorgen Schaefer
+;; Copyright (C) 2005, 2012  Jorgen Schaefer
 
 ;; Author: Jorgen Schaefer <forcer@forcix.cx>
 
@@ -8,23 +8,21 @@
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 2
+;; as published by the Free Software Foundation; either version 3
 ;; of the License, or (at your option) any later version.
-
+;; 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;; 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-;; 02110-1301  USA
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; An improved kind of formatting function for applications using Lui.
-;; This might eventually be included in lui.el
+;; An improved formatting function using named parameters.
+;;
 ;; See the docstring of `lui-format' for more details.
 
 ;;; Code:
@@ -32,9 +30,16 @@
 (require 'lui)
 
 (defun lui-display (format not-tracked-p &rest keywords)
-  "Insert FORMAT formatted with KEYWORDS in the current Lui buffer.
-See `lui-format' for a description of the arguments.
-When NOT-TRACKED-P is non-nil, this is not considered to be tracked."
+  "Display a formatted string in the current Lui interface.
+
+The string is formatted using FORMAT and `lui-format'.
+
+If NOT-TRACKED-P is given, the inserted string won't trigger
+tracking. See `lui-insert' for a description.
+
+KEYWORDS are the keyword arguments passed to `lui-format'.
+
+See `lui-format' for a full description of the arguments."
   (lui-insert (lui-format format keywords)
               not-tracked-p))
 
@@ -48,15 +53,31 @@ string, it is formatted according to the rules below.
 KEYWORDS is a plist of keywords and strings, or symbols and
 strings. They are used as format arguments.
 
-The string is taken verbatim, unless there is a backslash or a
-opening brace. A backslash before a character means it is taken
-literally. This is only necessary (but then also required) in
-front of braces or backslashes.
+The string is taken verbatim, unless there is are opening or
+closing braces.
 
-A word inside braces is looked up in the keywords plist. If the
-format is either a number, a number followed by a dash, or two
-numbers with a dash in between them, this is taken as a special
-format that is looked up in the list given using the list
+Double opening or closing braces are replaced by single
+occurrences of those characters. Otherwise, the contents between
+opening and closing braces is a format description and replaced
+by a formatted string.
+
+The string between opening and closing braces is taken as a name
+of a keyword argument, and replaced by that argument's value. If
+there is a colon in the string, the keyword name is the part
+before the colon. The part after the colon is used to format the
+argument using standard `format'
+
+Example:
+
+  (lui-format \"Hello {foo:.1f}\" :foo 3.1415)
+
+is equivalent to
+
+  (format \"Hello %.1f\" 3.1415)
+
+If the name is either a number, a number followed by a dash, or
+two numbers with a dash in between them, this is taken as a
+special name that is looked up in the list given using the list
 argument to the :indexed-args keyword.
 
 {1} refers to the second element (element 1)
@@ -68,57 +89,77 @@ by a single space character.
 
 All named arguments receive a property of `lui-format-argument'
 with the respective name as value. The whole string receives a
-`lui-format-type' property with the FORMAT symbol as a value."
-  (let ((str (if (stringp format)
-                 format
-                 (symbol-value format)))
-        (plist (mapcar (lambda (entry)
-                         (if (keywordp entry)
-                             ;; Keyword -> symbol
-                             (intern (substring (symbol-name entry)
-                                                1))
-                           entry))
-                       (if (and (not (null keywords))
-                                (null (cdr keywords)))
-                           (car keywords)
-                         keywords))))
-    (let ((val (if (functionp str)
-                   (funcall str plist)
-                 (lui-format-internal str plist))))
-      (if (symbolp format)
-          (propertize val 'lui-format-type format)
-        val))))
+`lui-format' property with FORMAT as a value, and a
+`lui-keywords' argument with KEYWORDS as a value."
+  ;; If it's only a single argument, that argument is a list.
+  (when (not (cdr keywords))
+    (setq keywords (car keywords)))
+  (if (functionp format)
+      (apply format keywords)
+    (let* ((format-string (if (symbolp format)
+                              (symbol-value format)
+                            format))
+           (plist (mapcar (lambda (entry)
+                            (if (keywordp entry)
+                                ;; Keyword -> symbol
+                                (intern (substring (symbol-name entry)
+                                                   1))
+                              entry))
+                          keywords)))
+      (propertize (lui-format-internal format-string plist)
+                  'lui-format format
+                  'lui-keywords keywords))))
 
 (defun lui-format-internal (fmt keywords)
-  "Format a prepared and normalized entry.
+  "Internal function for `lui-format'.
+
+FMT is the format string and KEYWORDS is the symbol-based plist.
+
 See `lui-format'."
   (with-temp-buffer
     (insert fmt)
     (goto-char (point-min))
-    (while (re-search-forward "\\(\\\\\\(.\\)\\|{\\([^}]+\\)}\\)" nil t)
-      (let ((quoted (match-string 2))
-            (tag (match-string 3)))
-        (if quoted
-            (replace-match quoted)
-          (replace-match (save-match-data
-                           (lui-format-single tag keywords))
-                         t t)
-          (add-text-properties (match-beginning 0)
-                               (point) ; Left there by `replace-match'
-                               `(lui-format-argument ,(intern tag))))))
+    (while (re-search-forward "{{\\|}}\\|{\\([^}]*\\)}" nil t)
+      (cond
+       ((string-equal (match-string 0) "3.1")
+        (replace-match "{"))
+       ((string-equal (match-string 0) "}}")
+        (replace-match "}"))
+       (t ;; (match-string 1)
+        (replace-match (save-match-data
+                         (lui-format-single (match-string 1) keywords))
+                       t t))))
     (buffer-string)))
 
-(defun lui-format-single (entry keywords)
-  "Format a single braced entry according to KEYWORDS.
+(defun lui-format-single (specifier keywords)
+  "Format a single braced SPECIFIER according to KEYWORDS.
+See `lui-format' for details.
+
+This adds `lui-format-argument' as necessary."
+  (let* ((split (split-string specifier ":"))
+         (identifier (car split))
+         (format (cadr split)))
+    (when (not format)
+      (setq format "s"))
+    (propertize (format (concat "%" format)
+                        (lui-format-lookup identifier keywords))
+                'lui-format-argument (intern identifier))))
+
+(defun lui-format-lookup (identifier keywords)
+  "Lookup the format IDENTIFIER in KEYWORDS.
+
 See `lui-format' for details."
   (cond
-   ((string-match "^\\([0-9]+\\)\\(-\\([0-9]+\\)?\\)?$" entry)
-    (let ((from (match-string 1 entry))
-          (rangep (match-string 2 entry))
-          (to (match-string 3 entry))
+   ((string-match "^\\([0-9]+\\)\\(-\\([0-9]+\\)?\\)?$" identifier)
+    (let ((from (match-string 1 identifier))
+          (rangep (match-string 2 identifier))
+          (to (match-string 3 identifier))
           (indexed-args (plist-get keywords 'indexed-args)))
       (if rangep
-          (mapconcat #'identity
+          (mapconcat (lambda (element)
+                       (if (stringp element)
+                           element
+                         (format "%s" element)))
                      (lui-sublist indexed-args
                                   (string-to-number from)
                                   (when to (string-to-number to)))
@@ -127,8 +168,8 @@ See `lui-format' for details."
                  indexed-args)
             (error "Index for format out of range: %S" from)))))
    (t
-    (or (plist-get keywords (intern entry))
-        (error "Unknown keyword argument %S" entry)))))
+    (or (plist-get keywords (intern identifier))
+        (error "Unknown keyword argument %S" identifier)))))
 
 (defun lui-sublist (list from &optional to)
   "Return the sublist from LIST starting at FROM and ending at TO."
