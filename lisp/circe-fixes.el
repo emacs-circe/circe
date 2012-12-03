e28ec5e3e8071538d4792b4303cd3d0d1a8ac641
Jorgen Schaefer <forcer@forcix.cx>
2012-12-03 18:58:22 +0100

    circe-fixes: Remove bugfix for minibuffer (included in 24.3)

--- lisp/circe-fixes.el
+++ lisp/circe-fixes.el
@@ -4,10 +4,4 @@
 (when (featurep 'xemacs)
   (require 'circe-fix-xemacs))
 
-;; completion-at-point and changing buffer
-;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=12619
-(when (and (require 'cl-lib nil t)
-           (not (boundp 'completion--all-sorted-completions-location)))
-  (require 'circe-fix-minibuffer))
-
 (provide 'circe-fixes)
