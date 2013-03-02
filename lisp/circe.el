e2137311a461feeeaab036dba9e10dd77f04d297
Jorgen Schaefer <forcer@forcix.cx>
2013-03-02 20:50:16 +0100

    circe.el: Typo in the description (also affects package README)

--- lisp/circe.el
+++ lisp/circe.el
@@ -32,7 +32,7 @@
 ;; Complexity-wise, it is somewhere between rcirc (very minimal) and ERC
 ;; (very complex).
 
-;; Once installed, you should be able to simple use M-x circe to
+;; Once installed, you should be able to simply use M-x circe to
 ;; connect to IRC. See the wiki on the project homepage for a more
 ;; elaboration configuration options.
 
