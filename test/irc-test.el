4fc9156c3d01cd787887cff6f9662dd0cefa3dfd
Jorgen Schaefer <contact@jorgenschaefer.de>
2015-03-27 18:01:02 +0100

    Test rework to use buttercup.

--- test/irc-test.el
+++ test/irc-test.el
@@ -1,63 +1,85 @@
+;; -*-lexical-binding: t-*-
+
 (require 'irc)
 
-(defmacro with-process (var &rest body)
-  (declare (indent 1))
-  `(let ((,var (start-process "test" nil "cat")))
-     (unwind-protect
-         (progn
-           ,@body)
-      (delete-process proc))))
+(describe "The irc--filter function"
+  (let (proc)
+    (before-each
+      (spy-on 'irc--handle-line)
+      (setq proc (start-process "test" nil "cat")))
 
-(ert-deftest test-irc--filter ()
-  "Make sure irc--filter handles multiple lines correctly."
-  (cl-letf* ((called-procs nil)
-             (called-lines nil)
-             ((symbol-function 'irc--handle-line)
-              (lambda (proc line)
-                (push proc called-procs)
-                (push line called-lines))))
-    ;; Single line
-    (with-process proc
+    (after-each
+      (ignore-errors
+        (delete-process proc)))
+
+    (it "should handle single lines"
       (irc--filter proc "line\n")
-      (should (equal called-procs (list proc)))
-      (should (equal called-lines (list "line"))))
 
-    (setq called-procs nil
-          called-lines nil)
-    ;; Multi-line
-    (with-process proc
+      (expect 'irc--handle-line
+              :to-have-been-called-with
+              proc "line"))
+
+    (it "should handle multiple lines at once"
       (irc--filter proc "line1\nline2\nline3\n")
-      (should (equal called-procs (list proc proc proc)))
-      (should (equal called-lines (list "line3" "line2" "line1"))))
 
-    (setq called-procs nil
-          called-lines nil)
-    ;; Partial line
-    (with-process proc
+      (expect (spy-calls-all-args 'irc--handle-line)
+              :to-equal
+              `((,proc "line1")
+                (,proc "line2")
+                (,proc "line3"))))
+
+    (it "should handle partial lines"
       (irc--filter proc "line1\nli")
-      (should (equal called-procs (list proc)))
-      (should (equal called-lines (list "line1")))
-      (irc--filter proc "ne2\n")
-      (should (equal called-procs (list proc proc)))
-      (should (equal called-lines (list "line2" "line1"))))
-    ))
 
-(ert-deftest test-irc--parse ()
-  "Parse IRC lines correctly."
-  (should (equal (irc--parse "COMMAND")
-                 '(nil "COMMAND")))
-  (should (equal (irc--parse "COMMAND arg")
-                 '(nil "COMMAND" "arg")))
-  (should (equal (irc--parse "COMMAND arg1 arg2")
-                 '(nil "COMMAND" "arg1" "arg2")))
-  (should (equal (irc--parse "COMMAND arg1 arg2 :arg3 still arg3")
-                 '(nil "COMMAND" "arg1" "arg2" "arg3 still arg3")))
-  (should (equal (irc--parse ":sender COMMAND")
-                 '("sender" "COMMAND")))
-  (should (equal (irc--parse ":sender COMMAND arg")
-                 '("sender" "COMMAND" "arg")))
-  (should (equal (irc--parse ":sender COMMAND arg1 arg2")
-                 '("sender" "COMMAND" "arg1" "arg2")))
-  (should (equal (irc--parse ":sender COMMAND arg1 arg2 :arg3 still arg3")
-                 '("sender" "COMMAND" "arg1" "arg2" "arg3 still arg3")))
-  )
+      (expect 'irc--handle-line
+              :to-have-been-called-with
+              proc "line1")
+
+      (spy-calls-reset 'irc--handle-line)
+
+      (irc--filter proc "ne2\n")
+
+      (expect 'irc--handle-line
+              :to-have-been-called-with
+              proc "line2"))))
+
+(describe "The `irc--parse' function"
+  (it "should parse a command without anything else"
+    (expect (irc--parse "COMMAND")
+            :to-equal
+            '(nil "COMMAND")))
+
+  (it "should parse a command with a single argument"
+    (expect (irc--parse "COMMAND arg")
+            :to-equal
+            '(nil "COMMAND" "arg")))
+
+  (it "should parse a command with two arguments"
+    (expect (irc--parse "COMMAND arg1 arg2")
+            :to-equal
+            '(nil "COMMAND" "arg1" "arg2")))
+
+  (it "should parse a command with rest argument"
+    (expect (irc--parse "COMMAND arg1 arg2 :arg3 still arg3")
+            :to-equal
+            '(nil "COMMAND" "arg1" "arg2" "arg3 still arg3")))
+
+  (it "should parse a command with sender and no arguments"
+    (expect (irc--parse ":sender COMMAND")
+            :to-equal
+            '("sender" "COMMAND")))
+
+  (it "should parse a command with sender and a single argument"
+    (expect (irc--parse ":sender COMMAND arg")
+            :to-equal
+            '("sender" "COMMAND" "arg")))
+
+  (it "should parse a command with sender and two arguments"
+    (expect (irc--parse ":sender COMMAND arg1 arg2")
+            :to-equal
+            '("sender" "COMMAND" "arg1" "arg2")))
+
+  (it "should parse a command with sender and rest argument"
+    (expect (irc--parse ":sender COMMAND arg1 arg2 :arg3 still arg3")
+            :to-equal
+            '("sender" "COMMAND" "arg1" "arg2" "arg3 still arg3"))))
---
+++ test/test-helper.el
@@ -0,0 +1,1 @@
+;; -*-lexical-binding: t-*-
