b317ebf5a575d1c5a60d1a9b8bee476239e2f8a9
Jorgen Schaefer <contact@jorgenschaefer.de>
2015-04-05 16:14:19 +0200

    Add commentary again, so the description shows up on melpa.org.

--- circe.el
+++ circe.el
@@ -22,6 +22,13 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Circe is a Client for IRC in Emacs. It integrates well with the rest
+;; of the editor, using standard Emacs key bindings and indicating
+;; activity in channels in the status bar so it stays out of your way
+;; unless you want to use it.
+
 ;;; Code:
 
 (defvar circe-version "1.6"
