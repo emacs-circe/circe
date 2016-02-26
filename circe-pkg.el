13a33ea7b3cc579cbf67db2109802df3366e84d1
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-02-26 17:37:43 +0100

    Bump version: 2.1 → 2.2

--- circe-pkg.el
+++ circe-pkg.el
@@ -1,3 +1,3 @@
-(define-package "circe" "2.1" "Client for IRC in Emacs"
+(define-package "circe" "2.2" "Client for IRC in Emacs"
   '((cl-lib "0.5"))
   :url "https://github.com/jorgenschaefer/circe")
--- circe.el
+++ circe.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2005 - 2015  Jorgen Schaefer
 
-;; Version: 2.1
+;; Version: 2.2
 ;; Keywords: IRC, chat
 ;; Author: Jorgen Schaefer <forcer@forcix.cx>
 ;; URL: https://github.com/jorgenschaefer/circe
@@ -31,7 +31,7 @@
 
 ;;; Code:
 
-(defvar circe-version "2.1"
+(defvar circe-version "2.2"
   "Circe version string.")
 
 (require 'circe-compat)
