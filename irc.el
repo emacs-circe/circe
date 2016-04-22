6223d1e6d0757d39fe56ae5d5d549c17cfb218da
Jorgen Schaefer <contact@jorgenschaefer.de>
2016-04-22 20:27:36 +0200

    Manually call the sentinel open event for blocking connects
    When Emacs connects to a server in blocking mode, the open event
    is never sent to the sentinel.
    
    Fixes #251

--- irc.el
+++ irc.el
@@ -70,23 +70,29 @@ conn.disconnected conn -- A previously established connection was lost
 
 NNN conn sender args... -- A numeric reply from IRC was received
 COMMAND conn sender args... -- An IRC command message was received"
-  (funcall (if (plist-get keywords :tls)
-               #'make-tls-process
-             #'make-network-process)
-           :name (or (plist-get keywords :name)
-                     (plist-get keywords :host))
-           :host (or (plist-get keywords :host)
-                     (error "Must specify a :host to connect to"))
-           :service (or (plist-get keywords :service)
-                        (error "Must specify a :service to connect to"))
-           :family (plist-get keywords :family)
-           :coding 'no-conversion
-           :nowait (featurep 'make-network-process '(:nowait t))
-           :noquery t
-           :filter #'irc--filter
-           :sentinel #'irc--sentinel
-           :plist keywords
-           :keepalive t))
+  (let ((proc (funcall (if (plist-get keywords :tls)
+                           #'make-tls-process
+                         #'make-network-process)
+                       :name (or (plist-get keywords :name)
+                                 (plist-get keywords :host))
+                       :host (or (plist-get keywords :host)
+                                 (error "Must specify a :host to connect to"))
+                       :service (or (plist-get keywords :service)
+                                    (error "Must specify a :service to connect to"))
+                       :family (plist-get keywords :family)
+                       :coding 'no-conversion
+                       :nowait (featurep 'make-network-process '(:nowait t))
+                       :noquery t
+                       :filter #'irc--filter
+                       :sentinel #'irc--sentinel
+                       :plist keywords
+                       :keepalive t)))
+    ;; When we used `make-network-process' without :nowait, the
+    ;; sentinel is not called with the open event, so we do this
+    ;; manually.
+    (when (eq (process-status proc) 'open)
+      (irc--sentinel proc "open manually"))
+    proc))
 
 (defun irc-connection-get (conn propname)
   "Return the value of CONN's PROPNAME property."
