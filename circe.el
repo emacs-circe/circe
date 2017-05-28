13c605e639194c3da0c2e685056fac685f8c76a0
Jorgen Schaefer <contact@jorgenschaefer.de>
2017-05-28 14:31:44 +0000

    Bump version: 2.4 → 2.5

--- circe.el
+++ circe.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2005 - 2015  Jorgen Schaefer
 
-;; Version: 2.4
+;; Version: 2.5
 ;; Keywords: IRC, chat
 ;; Author: Jorgen Schaefer <forcer@forcix.cx>
 ;; URL: https://github.com/jorgenschaefer/circe
@@ -31,7 +31,7 @@
 
 ;;; Code:
 
-(defvar circe-version "2.4"
+(defvar circe-version "2.5"
   "Circe version string.")
 
 (require 'circe-compat)
