6ccd4b494cbae9d28091217654f052eaea321007
Jorgen Schaefer <contact@jorgenschaefer.de>
2019-03-22 12:42:46 +0000

    Bump version: 2.10 → 2.11

--- circe-pkg.el
+++ circe-pkg.el
@@ -1,3 +1,3 @@
-(define-package "circe" "2.10" "Client for IRC in Emacs"
+(define-package "circe" "2.11" "Client for IRC in Emacs"
   '((cl-lib "0.5"))
   :url "https://github.com/jorgenschaefer/circe")
