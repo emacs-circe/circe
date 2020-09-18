8516160e407c07c81532566c2eb637c6be59ddb3
ag91 <agiugliano@live.it>
2020-09-18 20:55:31 +0200

    Hide tracking in modeline if tracking-max-mode-line-entries having = 0

--- tests/test-tracking.el
+++ tests/test-tracking.el
@@ -9,3 +9,48 @@
              (car (tracking-shorten
                    (list (propertize (buffer-name) 'face 'foo)))))
             :to-equal '(face foo))))
+
+(describe "The `tracking-status' function"
+  (it "should display 2 entries in the modeline if `tracking-max-mode-line-entries' is `nil'"
+    (let* ((tracking-buffers (list "someBuffer1" "someBuffer2"))
+           (buf1 (get-buffer-create (first tracking-buffers)))
+           (buf2 (get-buffer-create (second tracking-buffers)))
+           (tracking-max-mode-line-entries nil)
+           (test-trace (prin1-to-string (tracking-status))))
+      (expect
+       test-trace
+       :to-match "someBuffer1")
+      (expect
+       test-trace
+       :to-match "someBuffer2")
+      (kill-buffer buf1)
+      (kill-buffer buf2)))
+
+  (it "should display 1 entry in the modeline and a "+1" if `tracking-max-mode-line-entries' is `1'"
+      (let* ((tracking-buffers (list "someBuffer1" "someBuffer2"))
+             (buf1 (get-buffer-create (first tracking-buffers)))
+             (buf2 (get-buffer-create (second tracking-buffers)))
+             (tracking-max-mode-line-entries 1)
+             (test-trace (prin1-to-string (tracking-status))))
+        (expect
+         test-trace
+         :to-match "someBuffer1")
+        (expect
+         test-trace
+         :to-match "\\+1")
+        (expect
+         test-trace
+         :not :to-match "someBuffer2")
+        (kill-buffer buf1)
+        (kill-buffer buf2)))
+
+  (it "should display nothing in the modeline if `tracking-max-mode-line-entries' is `0'"
+    (let* ((tracking-buffers (list "someBuffer1" "someBuffer2"))
+           (buf1 (get-buffer-create (first tracking-buffers)))
+           (buf2 (get-buffer-create (second tracking-buffers)))
+           (tracking-max-mode-line-entries 0))
+      (expect
+       (tracking-status)
+       :to-equal "")
+      (kill-buffer buf1)
+      (kill-buffer buf2))))
