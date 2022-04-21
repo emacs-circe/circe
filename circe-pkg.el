1d15791afe834539be372b1ae18c185d858b2821
Vasilij Schneidermann <mail@vasilij.de>
2022-04-21 21:50:17 +0200

    Bump minimum supported Emacs version to 24.5

--- circe-pkg.el
+++ circe-pkg.el
@@ -1,3 +1,3 @@
 (define-package "circe" "2.11" "Client for IRC in Emacs"
-  '((emacs "24.4") (cl-lib "0.5"))
+  '((emacs "24.5") (cl-lib "0.5"))
   :url "https://github.com/emacs-circe/circe")
--- tests/test-irc.el
+++ tests/test-irc.el
@@ -261,9 +261,6 @@
             '(nil "PRIVMSG" "#channel" "möp")))
 
   (it "should decode arguments individually"
-    ;; Undecided is broken in older Emacsen
-    (when (version< emacs-version "24.4")
-      (signal 'buttercup-pending t))
     ;; This is utf-16
     (expect (irc--parse
              (concat ":\xff\xfe\x6d\x00\xf6\x00\x70\x00 "
